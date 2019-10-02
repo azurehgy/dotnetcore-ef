dba837f139047101181eb3f75621cb08d955847f
Azure Hgy <azurehgy@zoho.com>
2019-10-02 10:08:37 +0000

    initial commit

---
+++ EFDotnetcore/Program.cs
@@ -0,0 +1,34 @@
+﻿using EFDotnetcore.Models;
+using System;
+using System.Linq;
+
+namespace EFDotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (var context = new SchoolContext())
+            //{
+
+            //    var std = new Student()
+            //    {
+            //        Name = "Bill"
+            //    };
+
+            //    context.Students.Add(std);
+            //    context.SaveChanges();
+            //}
+            var context = new SchoolContext();
+            var studentsWithSameName = context.Students
+                                              .Where(s => s.Name == GetName())
+                                              .ToList();
+            Console.WriteLine(studentsWithSameName[0].StudentId.ToString());
+        }
+
+        public static string GetName()
+        {
+            return "Bill";
+        }
+    }
+}
---
+++ EFDotnetcore/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using EFDotnetcore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDotnetcore
+{
+    public class SchoolContext : DbContext
+    {
+        private const string dbconnstr = @"Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;";
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(dbconnstr);
+        }
+    }
+}
